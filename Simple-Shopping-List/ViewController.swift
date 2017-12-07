cb23d8a1af7b664d8acf0d8211ae0a601a47641a
YASH SOMPURA <yash007@yashs-macbook-pro.local>
2017-12-06 19:49:02 -0500

    Title and Basic Label added

--- Simple-Shopping-List/ViewController.swift
+++ Simple-Shopping-List/ViewController.swift
@@ -1,25 +1,18 @@
-//
-//  ViewController.swift
-//  Simple-Shopping-List
-//
-//  Created by YASH SOMPURA on 2017-12-06.
-//  Copyright © 2017 YASH SOMPURA. All rights reserved.
-//
+/*
+     Project Name: Simple Shopping List
+     Purpose: Mid Term Exam
+     Students: Yashkumar Sompura - 300967186
+                Shivam Shah      - 300877523
+     Test Date : 6 Dec, 2017
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
