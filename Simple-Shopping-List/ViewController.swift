65044317c8c24db2b980dde25a5d4d6ed2a77014
YASH SOMPURA <yash007@yashs-macbook-pro.local>
2017-12-06 20:58:04 -0500

    Reset Button Function Added!!

--- Simple-Shopping-List/ViewController.swift
+++ Simple-Shopping-List/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //stepper button listed here
+    @IBOutlet weak var firstStepper: UIStepper!
+    
+    
+    //stepper counter listed here
+    @IBOutlet weak var firstStepperLabel: UILabel!
+    
+    @IBOutlet weak var secondStepperLabel: UILabel!
+    @IBOutlet weak var thirdStepperLabel: UILabel!
+    @IBOutlet weak var fourthStepperLabel: UILabel!
+    @IBOutlet weak var fifthStepperLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func firstStepperAction(_ sender: Any) {
+        firstStepperLabel.text = "\(Int(firstStepper.value))"
+    }
+    
+    @IBAction func resetQuantity(_ sender: Any) {
+        firstStepperLabel.text = "1"
+        secondStepperLabel.text = "1"
+        thirdStepperLabel.text = "1"
+        fourthStepperLabel.text = "1"
+        fifthStepperLabel.text = "1"
+    }
 }
 
